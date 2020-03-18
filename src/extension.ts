847bf5a4338f330f421ec6da233ca3c8acec038b
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-18 09:33:35 +0800

    is working

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import {LogLevel} from './constants';
+import { LogLevel } from './constants';
 import { Logger } from './logger';
+import { ForceComment } from './forcecomment';
+
 var logger = new Logger(LogLevel.INFO);
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-forcecomment" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.forcecomment.startForceComment', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World! Please Comment from time to time!');
-	});
-
-	context.subscriptions.push(disposable);
+	let forcecomment = new ForceComment(context.extensionPath, logger);
+	
+	context.subscriptions.push(vscode.commands.registerCommand('extension.forcecomment.startForceComment', () => {
+		vscode.window.showInformationMessage(' Please Comment from time to time!');
+	}));
 
 	let hitcomment = vscode.commands.registerCommand('extension.forcecomment.hitComment', () => {
 		let editor = vscode.window.activeTextEditor as vscode.TextEditor;
 		editor.edit(edit => {
 			edit.insert(editor.selection.active, '/');
 		});
-		vscode.window.showInformationMessage('You hit \/!');
+		forcecomment.clearHeartbeat();
+		// vscode.window.showInformationMessage('You hit \/!');
 	});
 
 	context.subscriptions.push(hitcomment);
-
+	context.subscriptions.push(forcecomment);
+	forcecomment.initialize();
 }
 
 // this method is called when your extension is deactivated
